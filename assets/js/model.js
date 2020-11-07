cc8abdd91b5422d45ef2b08bc665de86bcb50b97
apyd <arturpydych@gmail.com>
2020-11-07 16:05:18 +0100

    Generating coordinates for new tile

--- assets/js/model.js
+++ assets/js/model.js
@@ -1,10 +1,28 @@
+import {
+    view
+} from './app.js';
 export class Model {
     constructor() {
-        this.board = [
-            ['', '', '', ''],
-            ['', '', '', ''],
-            ['', '', '', ''],
-            ['', '', '', '']
-        ];
-    }
+        this.score;
+        this.bestScore;
+        this.board = [['','','',''],
+                      ['','','',''],
+                      ['','','',''],
+                      ['','','','']]
+        }
+        generateCoordinates() {
+            let x = Math.floor((Math.random() * 4) + 1);
+            let y = Math.floor((Math.random() * 4) + 1);
+            this.checkColision(x,y)
+            console.log(x, y);
+            return `${x}${y}`;
+        }
+
+        checkColision(x,y) {
+            
+        }
+
+        moveTiles() {
+                console.log('moveTiles');
+        }
 }
