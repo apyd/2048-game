64edaad252070a1b8d4e44228ab8ccb31445247f
apyd <arturpydych@gmail.com>
2020-11-17 22:40:39 +0100

    Merge tiles

--- assets/js/model.js
+++ assets/js/model.js
@@ -35,17 +35,16 @@ export class Model {
             (key === 'ArrowUp' || key === 'ArrowDown') ? boardToSort = this.rotateBoard([...boardToSort]): null;
             if (key === 'ArrowRight' || key === 'ArrowDown') {
                 for (let [index, subArray] of boardToSort.entries()) {
-                    sortedBoard[index] = subArray.filter(el => !el).concat(subArray.filter(el => el));
+                    sortedBoard[index] = subArray.filter(el => !el).concat(this.mergeTiles(subArray.filter(el => el), key));
                 }
             }
             else {
                 for (let [index, subArray] of boardToSort.entries()) {
-                    sortedBoard[index] = subArray.filter(el => el).concat(subArray.filter(el => !el));
+                    sortedBoard[index] = this.mergeTiles(subArray.filter(el => el), key).concat(subArray.filter(el => !el));
                 }
             }
             (key === 'ArrowUp' || key === 'ArrowDown') ? sortedBoard = this.rotateBoard([...sortedBoard]): null;
             this.board = [...sortedBoard];
-            console.log(this.board);
         }
 
         rotateBoard(boardToRotate) {
@@ -57,4 +56,22 @@ export class Model {
             }
             return rotatedBoard;
         }
-    }
+        mergeTiles(arr, key) {
+            if ((key === 'ArrowDown' || key === 'ArrowRight')) {
+                for (let i = arr.length; i > 0; i--) {
+                    if (arr[i] === arr[i - 1]) {
+                        arr[i] += arr[i - 1];
+                        arr[i - 1] = '';
+                    }
+                }
+            } else {
+                for (let i = 0; i < arr.length; i++) {
+                    if (arr[i] === arr[i + 1]) {
+                        arr[i] += arr[i + 1];
+                        arr[i + 1] = '';
+                    }
+                }
+            }
+            return arr;
+        }
+}
