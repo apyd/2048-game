7a65ee329e14314b6ba1c8aab27aeae657029b9d
apyd <arturpydych@gmail.com>
2020-12-06 19:41:10 +0100

    Entry screen with game type selection

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,10 +8,17 @@ const startBtn = document.querySelectorAll(".btn--primary")[0];
 const closeBtn = document.querySelector(".btn--close");
 const rulesBtn = document.querySelectorAll(".btn--primary")[1];
 const overlay = document.querySelector(".overlay");
+const radioBtns = document.querySelectorAll(".radio");
 
 export const view = new View();
 export const model = new Model();
 export const controller = new Controller();
+
+radioBtns.forEach(radioBtn => radioBtn.addEventListener('click', () => {
+    startBtn.disabled = false;
+    controller.gameType(radioBtn.value);
+}))
+
 startBtn.addEventListener("click", () => controller.startGame());
 rulesBtn.addEventListener("click", () => view.togglePopup());
 closeBtn.addEventListener("click", () => view.togglePopup());
