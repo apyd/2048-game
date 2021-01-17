0fa71c4df78a576f0fc6e8b5a3a3cd946fd13866
apyd <arturpydych@gmail.com>
2021-01-17 14:34:17 +0100

    ESLint rules update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
 	env: {
 		es6: true,
-		browser: true,
+		browser: true
 	},
 	extends: [
-		'airbnb-base',
+		'airbnb-base'
 	],
 	parser: '@babel/eslint-parser',
 	parserOptions: {
 		ecmaVersion: 6,
-		sourceType: 'module',
+		sourceType: 'module'
 	},
 	rules: {
 		indent: ['error', 'tab'],
@@ -17,5 +17,15 @@ module.exports = {
 		'eol-last': 0,
 		'import/extensions': 0,
 		'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
-	},
+		'no-param-reassign': ['error', { props: false }],
+		'comma-dangle': ['error', {
+			arrays: 'never',
+			objects: 'never',
+			imports: 'never',
+			exports: 'never',
+			functions: 'never'
+		}],
+		'max-len': ['error', { code: 125 }],
+		'prefer-exponentiation-operator': 0
+	}
 };
