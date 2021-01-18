04eac0245cc85f50d5d652ec492826d54f65d8d8
apyd <arturpydych@gmail.com>
2021-01-18 20:54:23 +0100

    Model refactor

--- assets/js/model.js
+++ assets/js/model.js
@@ -1,5 +1,6 @@
+import keys from './keys.js';
 import { controller, view } from './app.js';
-import { generateRandomCoordinates } from './utils.js';
+import { generateRandomCoordinates, checkIfArraysEqual } from './utils.js';
 
 export default class Model {
 	bestScore;
@@ -15,155 +16,144 @@ export default class Model {
 
 	initializeGame(selectedType) {
 		this.gameType = +selectedType;
-		this.board = this.createBoard();
+		this.clearBoard();
 		this.bestScore = +localStorage.getItem(`bestScore${this.gameType}`);
-	};
+	}
 
 	createBoard(boardType) {
 		switch (boardType) {
-			case 1:
-				return Array(this.gameType).fill().map(() => Array());
-			case 2:
-				return Array(this.gameType).fill().map(() => Array(this.gameType));
-			default:
-				return Array(this.gameType).fill().map(() => Array(this.gameType).fill(''));
+		case 1:
+			return Array(this.gameType)
+				.fill()
+				.map(() => []);
+		case 2:
+			return Array(this.gameType)
+				.fill()
+				.map(() => Array(this.gameType));
+		default:
+			return Array(this.gameType)
+				.fill()
+				.map(() => Array(this.gameType).fill(''));
 		}
-	};
+	}
 
 	clearBoard() {
 		this.board = this.createBoard();
 		this.nextAvailableTileId = 0;
 		this.score = 0;
-	};
+	}
 
 	checkCollision({ x, y }) {
-		return !!this.board[y][x] ? false : { x, y };
-	};
+		return this.board[y][x] ? false : { x, y };
+	}
 
 	createTile() {
 		return {
 			id: this.nextAvailableTileId,
 			val: 2
-		}
+		};
 	}
 
 	addTileToBoard() {
-		if (!this.canAddTile) return; 
+		if (!this.canAddTile) return false;
 		const tile = this.createTile();
 		let tileCoordinates;
 		while (!tileCoordinates) {
 			tileCoordinates = this.checkCollision(generateRandomCoordinates(this.gameType));
 		}
 		this.board[tileCoordinates.y][tileCoordinates.x] = tile;
-		this.nextAvailableTileId++;
+		this.nextAvailableTileId += 1;
 		controller.incrementNumberOfTilesOnBoard();
 		return {
 			y: tileCoordinates.y,
 			x: tileCoordinates.x,
 			id: tile.id
 		};
-	};
+	}
 
 	moveTiles(key) {
 		let boardToSort = [...this.board];
-		let sortedBoard = this.createBoard(2);
-		(key === 'ArrowUp' || key === 'ArrowDown') ? boardToSort = this.rotateBoard([...boardToSort]): null;
-		if (key === 'ArrowRight' || key === 'ArrowDown') {
-			for (let [index, subArray] of boardToSort.entries()) {
-				sortedBoard[index] = subArray.filter(el => !el).concat(this.mergeTiles(subArray.filter(el => el), key));
-				sortedBoard[index] = [...sortedBoard[index]].filter(el => !el).concat([...sortedBoard[index]].filter(el => el));
-			}
-		}
-		else {
-			for (let [index, subArray] of boardToSort.entries()) {
-				sortedBoard[index] = this.mergeTiles(subArray.filter(el => el), key).concat(subArray.filter(el => !el));
-				sortedBoard[index] = [...sortedBoard[index]].filter(el => el).concat([...sortedBoard[index]].filter(el => !el));
-			}
-		}
-		(key === 'ArrowUp' || key === 'ArrowDown') ? sortedBoard = this.rotateBoard([...sortedBoard]): null;
-		let previouslyUpdatedTiles = this.generateArrayWithUpdatedTiles(this.board);
+		boardToSort = (key === keys.ArrowUp || key === keys.ArrowDown) ? this.rotateBoard([...boardToSort]) : boardToSort;
+		let sortedBoard = boardToSort.map((boardRow) => Model.sortArray(this.mergeTiles(boardRow, key), key));
+		sortedBoard = (key === keys.ArrowUp || key === keys.ArrowDown) ? this.rotateBoard([...sortedBoard]) : sortedBoard;
+		const previouslyUpdatedTiles = Model.generateArrayWithUpdatedTiles(this.board);
 		this.board = [...sortedBoard];
-		let arrWithUpdatedTiles = this.generateArrayWithUpdatedTiles(sortedBoard);
-		this.canAddTile = !this.checkIfArraysEqual(previouslyUpdatedTiles, arrWithUpdatedTiles);
-		if (!this.canAddTile) return;
-		this.numberOfMoves++;
+		const arrWithUpdatedTiles = Model.generateArrayWithUpdatedTiles(sortedBoard);
+		this.canAddTile = !checkIfArraysEqual(previouslyUpdatedTiles, arrWithUpdatedTiles);
+		if (!this.canAddTile) return false;
+		this.numberOfMoves += 1;
 		return arrWithUpdatedTiles;
-	};
+	}
 
-	checkIfPossibleMerge() {
-		let tempBoard = [...this.board];
-		for (const subArray of tempBoard) {
-			for (let i = 0; i <= subArray.length - 2; i++) {
-				if (subArray[i].val === subArray[i + 1].val) return true;
-			}
+	static sortArray(arrayToSort, key) {
+		if (key === keys.ArrowLeft || key === keys.ArrowUp) {
+			return ((arrayToSort.filter((arrayElement) => arrayElement)))
+				.concat(arrayToSort.filter((arrayElement) => !arrayElement));
 		}
-		tempBoard = this.rotateBoard([...tempBoard]);
-		for (const subArray of tempBoard) {
-			for (let i = 0; i <= subArray.length - 2; i++) {
-				if (subArray[i].val === subArray[i + 1].val) return true;
-			}
-		}
-		return false;
-	};
+		return ((arrayToSort.filter((arrayElement) => !arrayElement)))
+			.concat(arrayToSort.filter((arrayElement) => arrayElement));
+	}
 
-	checkIfArraysEqual(arr1, arr2) {
-		if (!!(arr1 && arr2)) return false;
-		let previouslyUpdatedTiles = JSON.stringify(arr1);
-		let arrWithUpdatedTiles = JSON.stringify(arr2);
-		return previouslyUpdatedTiles === arrWithUpdatedTiles
-	};
-
-	generateArrayWithUpdatedTiles(board) {
-		let arrayWithTiles = [];
-		for (let [index, subArray] of board.entries()) {
-			subArray.filter(el => el).map(el => {
-				arrayWithTiles.push({
-					id: el.id,
-					val: el.val,
-					y: index,
-					x: subArray.map(arrayEl => { return arrayEl.id }).indexOf(el.id),
-					mergedId: el.mergedId
-				});
-			});
-		}
-		return arrayWithTiles;
-	};
+	static generateArrayWithUpdatedTiles(board) {
+		const arrayWithUpdatedTiles = [];
+		board.forEach((boardRow, index) => {
+			boardRow.filter((tile) => tile).map((tile) => arrayWithUpdatedTiles.push({
+				id: tile.id,
+				val: tile.val,
+				y: index,
+				x: boardRow.map((arrayElement) => arrayElement.id).indexOf(tile.id),
+				mergedId: tile.mergedId
+			}));
+		});
+		return arrayWithUpdatedTiles;
+	}
 
 	rotateBoard(boardToRotate) {
-		let rotatedBoard = this.createBoard(1);
+		const rotatedBoard = this.createBoard(1);
 		for (let i = 0; i < boardToRotate.length; i++) {
 			for (let j = 0; j < boardToRotate.length; j++) {
 				rotatedBoard[i].push(boardToRotate[j][i]);
 			}
 		}
 		return rotatedBoard;
-	};
+	}
 
-	mergeTiles(arr, key) {
-		if (arr.length < 2) return arr;
-		if ((key === 'ArrowDown' || key === 'ArrowRight')) {
-			for (let i = arr.length - 1; i >= 1; i--) {
-				if (arr[i].val === arr[i - 1].val) {
-					arr[i].val += arr[i - 1].val;
-					arr[i].mergedId = arr[i - 1].id;
-					arr[i - 1] = '';
-					this.updateScore(arr[i].val);
-					controller.tilesOnBoard--;
+	checkIfPossibleMerge() {
+		const board = [...this.board];
+		board.forEach((boardRow) => {
+			for (let i = 0; i <= boardRow.length - 2; i++) {
+				if (boardRow[i].val === boardRow[i + 1].val) return true;
+			}
+			return false;
+		});
+	}
+
+	mergeTiles(array, key) {
+		const filteredArray = array.filter((el) => el);
+		if (filteredArray.length < 2) return array;
+		if (key === keys.ArrowDown || key === keys.ArrowRight) {
+			for (let i = filteredArray.length - 1; i >= 1; i--) {
+				if (filteredArray[i].val === filteredArray[i - 1].val) {
+					filteredArray[i].val += filteredArray[i - 1].val;
+					filteredArray[i].mergedId = filteredArray[i - 1].id;
+					filteredArray[i - 1] = '';
+					this.updateScore(filteredArray[i].val);
+					controller.decrementNumberOfTilesOnBoard();
 				}
 			}
 		} else {
-			for (let i = 0; i <= arr.length - 2; i++) {
-				if (arr[i].val === arr[i + 1].val) {
-					arr[i].val += arr[i + 1].val;
-					arr[i].mergedId = arr[i + 1].id;
-					arr[i + 1] = '';
-					this.updateScore(arr[i].val);
+			for (let i = 0; i <= filteredArray.length - 2; i++) {
+				if (filteredArray[i].val === filteredArray[i + 1].val) {
+					filteredArray[i].val += filteredArray[i + 1].val;
+					filteredArray[i].mergedId = filteredArray[i + 1].id;
+					filteredArray[i + 1] = '';
+					this.updateScore(filteredArray[i].val);
 					controller.decrementNumberOfTilesOnBoard();
 				}
 			}
 		}
-		return arr;
-	};
+		return (array.filter((el) => !el)).concat(filteredArray);
+	}
 
 	updateScore(pointsToAdd) {
 		this.score += pointsToAdd;
@@ -172,5 +162,5 @@ export default class Model {
 			this.bestScore = +localStorage.getItem(`bestScore${this.gameType}`);
 		}
 		view.updateScore(this.score);
-	};
+	}
 }
