be62bce10ce763262ff36d23c6839aaca9f6e32e
apyd <arturpydych@gmail.com>
2021-01-18 20:54:37 +0100

    App.js refactor

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,32 @@
-import Controller from "./controller.js";
-import View from "./view.js";
-import Model from "./model.js";
+import { createModelInstance, createViewInstance, createControllerInstance } from './init.js';
 
-const startBtn = document.getElementById("start-game");
+const startBtn = document.getElementById('start-game');
 const restartBtn = document.getElementById('restart-game');
-const closeBtn = document.getElementById("close-popup");
-const arrowBtn = document.getElementById("open-entry-screen");
-const overlay = document.querySelector(".overlay");
-const radioBtns = document.querySelectorAll(".game-option__input");
-const board = document.querySelector(".board");
+const closeBtn = document.getElementById('close-popup');
+const arrowBtn = document.getElementById('open-entry-screen');
+const overlay = document.querySelector('.overlay');
+const radioBtns = document.querySelectorAll('.game-option__input');
+const board = document.querySelector('.board');
 
-export const view = new View();
-export const model = new Model();
-export const controller = new Controller();
+export const view = createViewInstance();
+export const model = createModelInstance();
+export const controller = createControllerInstance();
 
-radioBtns.forEach(radioBtn => radioBtn.addEventListener('click', () => {
+radioBtns.forEach((radioBtn) => radioBtn.addEventListener('click', () => {
 	startBtn.disabled = false;
 	controller.gameType = radioBtn.value;
-}))
-
-startBtn.addEventListener("click", () => controller.startGame());
-restartBtn.addEventListener("click", () => controller.startGame());
-closeBtn.addEventListener("click", () => view.togglePopup());
-arrowBtn.addEventListener('click', () => controller.stopGame());
-overlay.addEventListener("click", (e) => e.target === overlay ? view.togglePopup() : null);
-window.addEventListener("keyup", (e) => controller.onKeyPress(e.key));
+}));
+startBtn.addEventListener('click', (e) => controller.startGame(e));
+restartBtn.addEventListener('click', (e) => controller.startGame(e));
+closeBtn.addEventListener('click', () => view.togglePopup());
+arrowBtn.addEventListener('click', () => controller.cancelGame(e));
+overlay.addEventListener('click', (e) => { if (e.target === overlay) view.togglePopup(); });
+window.addEventListener('keyup', (e) => controller.onKeyPress(e.key));
 window.addEventListener('DOMContentLoaded', () => view.updateScore());
-board.addEventListener('touchstart', (e) => controller.onTouch(e.type, e.changedTouches[0].clientX, e.changedTouches[0].clientY));
-board.addEventListener('touchend', (e) => controller.onTouch(e.type, e.changedTouches[0].clientX, e.changedTouches[0].clientY));
-window.addEventListener('resize', (e) => view.onScreenResize());
+board.addEventListener('touchstart', (e) => {
+	controller.onTouch(e.type, e.changedTouches[0].clientX, e.changedTouches[0].clientY);
+});
+board.addEventListener('touchend', (e) => {
+	controller.onTouch(e.type, e.changedTouches[0].clientX, e.changedTouches[0].clientY);
+});
+window.addEventListener('resize', () => view.onScreenResize());
